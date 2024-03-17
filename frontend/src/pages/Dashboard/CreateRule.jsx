f6e41d66b6a1efd1166204a69cbe88840110f638
Harshit Yadav <2105891@kiit.ac.in>
2024-03-17 17:47:31 +0530

    feat: Tooltip, rule page info

--- frontend/src/pages/Dashboard/CreateRule.jsx
+++ frontend/src/pages/Dashboard/CreateRule.jsx
@@ -4,6 +4,14 @@ import { propertyImportFromDB } from "../../lib/propertyImportFromDB";
 import { Button } from "@/components/ui/button";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { IoMdInformationCircleOutline } from "react-icons/io";
+
 const CreateRule = () => {
 
     const [ruleFormData, setRuleFormData] = useState({
@@ -124,32 +132,66 @@ const CreateRule = () => {
 
     return (
         <section className="p-8">
+
+            <div className="w-full bg-[#f0f8ff] rounded-lg p-8 flex justify-start items-start gap-4">
+                <div>
+                    <img src="/idea-bulb.png" alt="" className="w-10 h-10" />
+                </div>
+                <div>
+                    <p className="text-sm font-light">This page is for creating the rules mannually.
+                    </p>
+                    <br />
+                    <p className="text-sm font-light">
+
+                        In this DecisionDriver, you have to enter following details to create a rule:
+                        <ul>
+                            <li >
+                                1. <span className="font-semibold">Name :</span> Name of the rule
+                                For eg, &quot;Under-age and high income.&quot;
+                            </li>
+                            <li>
+                                2. <span className="font-semibold">Description :</span> Description about the rule.
+                                For eg, &quot;Applicant is not under-age, and has a high income.&quot;
+                            </li>
+                            <li>
+                                3. <span className="font-semibold">Set Property:</span> Select a property or variable to base the rule on.
+                            </li>
+                            <li>
+                                3. <span className="font-semibold">Set Operator:</span> Select a operator to compare the value.
+                            </li>
+                            <li>
+                                4. <span className="font-semibold">Set Value:</span> Write the numeric value to compare.
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+            </div>
             <form onSubmit={submitHandler}>
                 <div className="w-full flex gap-4">
-
                     <div>
                         <label htmlFor="name">Name</label>
                         <br />
-                        <input type="text" id="name" className="border-2 input-styles " name="name" value={ruleFormData.name} onChange={(e) => setRuleFormData({ ...ruleFormData, name: e.target.value })} />
+                        <input type="text" id="name" placeholder="Rule Name" className=" input-styles " name="name" value={ruleFormData.name} onChange={(e) => setRuleFormData({ ...ruleFormData, name: e.target.value })} />
+                        <br />
                     </div>
                     <div>
 
                         <label htmlFor="description">Description</label>
                         <br />
-                        <input type="text" id="description" className="border-2 w-[30vw] input-styles" name="description" value={ruleFormData.description} onChange={(e) => setRuleFormData({ ...ruleFormData, description: e.target.value })} />
+                        <input type="text" id="description" className=" w-[30vw] input-styles" name="description" value={ruleFormData.description} placeholder="Description" onChange={(e) => setRuleFormData({ ...ruleFormData, description: e.target.value })} />
                     </div>
-                    <div className="w-1/3">
+                    {/* <div className="w-1/3">
                         <label htmlFor="tested">Tested</label>
                         <br />
                         <input type="checkbox" id="tested" name="tested" checked={ruleFormData.tested} onChange={(e) => setRuleFormData({ ...ruleFormData, tested: e.target.checked })} />
-                    </div>
+                    </div> */}
                 </div>
 
-                <h1 className="text-xl font-bold">Rules</h1>
+                <h1 className="text-xl font-bold mt-8">Rules</h1>
                 {/* dynamic form */}
                 {ruleFormData.conditionSchema.map((item, index) => (
-                    <div key={index} className="w-full flex gap-4 justify-center items-end border-2">
-                        <div className="w-1/4 border-2">
+                    <div key={index} className="w-full flex gap-4 justify-center items-end ">
+                        <div className="w-1/4 ">
                             <label htmlFor={`property-${index}`}>Property</label>
                             <br />
                             <select className="input-styles w-full" name={`property-${index}`} id={`operator-${index}`} onChange={(e) => handleSchemaChange("conditionSchema", index, 'property', e.target.value)}>
@@ -182,7 +224,6 @@ const CreateRule = () => {
                         <div className="w-1/4">
                             <label htmlFor={`connectedBy-${index}`}>Connected By</label>
                             <br />
-                            {/* <input type="text" id={`connectedBy-${index}`} className="border-2 input-styles w-full" name={`connectedBy-${index}`} value={item.connectedBy} onChange={(e) => handleSchemaChange("conditionSchema", index, 'connectedBy', e.target.value)} /> */}
                             <select className="input-styles w-full" name={`connectedBy-${index}`} id={`connectedBy-${index}`} onChange={(e) => handleSchemaChange("conditionSchema", index, 'connectedBy', e.target.value)}>
                                 {connectedBy.map((op, index) => {
                                     return (
@@ -191,7 +232,17 @@ const CreateRule = () => {
                                 })}
                             </select>
                         </div>
-                        <Button type="button" onClick={addFieldsConditionSchema} className=" text-white">Add Fields +</Button>
+                        <TooltipProvider>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button type="button" onClick={addFieldsConditionSchema} className=" text-white"> <IoMdInformationCircleOutline size={20} /> &nbsp; Add Fields +</Button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Add more fileds to combine conditions.</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+
                         <Button type="button" disabled variant="destructive" onClick={addFieldsConditionSchema} className=" text-white">Delete Fields </Button>
 
                         {/* button for nesting conditions */}
@@ -207,11 +258,11 @@ const CreateRule = () => {
                 {/* for action schema */}
                 {ruleFormData.actionSchema.map((item, index) => {
                     return (
-                        <div key={index} className="w-full flex gap-4 justify-center items-end border-2">
+                        <div key={index} className="w-full flex gap-4 justify-center items-end ">
                             <div className="w-1/3">
                                 <label htmlFor={`property-${index}`}>Property</label>
                                 <br />
-                                {/* <input type="text" id={`property-${index}`} className="border-2 input-styles w-full" name={`property-${index}`} value={item.property} onChange={(e) => handleSchemaChange("actionSchema", index, 'property', e.target.value)} /> */}
+                                {/* <input type="text" id={`property-${index}`} className=" input-styles w-full" name={`property-${index}`} value={item.property} onChange={(e) => handleSchemaChange("actionSchema", index, 'property', e.target.value)} /> */}
                                 <select className="input-styles w-full" name={`property-${index}`} id={`operator-${index}`} onChange={(e) => handleSchemaChange("actionSchema", index, 'property', e.target.value)}>
                                     {property.map((op, index) => {
                                         return (
@@ -224,7 +275,7 @@ const CreateRule = () => {
                             <div className="w-1/3">
                                 <label htmlFor={`result-${index}`}>Result</label>
                                 <br />
-                                <input type="text" id={`result-${index}`} className="border-2 input-styles w-full" name={`result-${index}`} value={item.result} onChange={(e) => handleSchemaChange("actionSchema", index, 'result', e.target.value)} />
+                                <input type="text" id={`result-${index}`} className=" input-styles w-full" name={`result-${index}`} value={item.result} onChange={(e) => handleSchemaChange("actionSchema", index, 'result', e.target.value)} />
                             </div>
                             <div className="w-1/3">
                                 <label htmlFor={`connectedBy-${index}`}>Connected By</label>
@@ -250,7 +301,7 @@ const CreateRule = () => {
 
                 <br />
                 <br />
-                <Button type="submit" className="border-2">Create Rule</Button>
+                <Button type="submit" className="">Create Rule</Button>
             </form>
             <ToastContainer />
         </section>
